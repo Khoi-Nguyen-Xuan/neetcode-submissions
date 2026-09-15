5f46759fb727745a8c04fd99336d07c3ed885582
Khoi-Nguyen-Xuan <107749708+Khoi-Nguyen-Xuan@users.noreply.github.com>
2026-09-15 10:20:26 +0900

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        tracking_dict = {}
+        for num in nums:
+            if num not in tracking_dict:
+                tracking_dict[num] = 0
+            tracking_dict[num] += 1 
+        
+        result_values = sorted(list(tracking_dict.values()), reverse=True)[:k]
+        matching_keys = [k for k,v in tracking_dict.items() if v in result_values] 
+
+        return matching_keys
+
+        
