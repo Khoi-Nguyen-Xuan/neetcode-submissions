cd273a367ea975695b0c5dd32c2f0603d336efd4
Khoi-Nguyen-Xuan <107749708+Khoi-Nguyen-Xuan@users.noreply.github.com>
2026-09-16 13:14:03 +0900

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        largest_l = []
+        largest_r = [0]*len(height) 
+        volume = 0
+
+        max_left = 0
+        for i in range(len(height)):
+            if height[i] > max_left: max_left=height[i]
+            largest_l.append(max_left)
+        
+        max_right = 0
+        for i in range(len(height)-1, -1, -1):
+            if height[i] > max_right: max_right=height[i]
+            largest_r[i] = max_right 
+        
+        for i in range(len(height)): 
+            volume += min(largest_l[i], largest_r[i]) - height[i]
+        
+        return volume 
+
+
